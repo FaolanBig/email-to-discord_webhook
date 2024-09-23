41bcb17860e9770ba78099c731c9816dcf4e94ae
FaolanBig <93519016+FaolanBig@users.noreply.github.com>
2024-09-23 18:00:49 +0200

    Program.cs aktualisieren

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
-﻿using MailKit.Net.Imap;
+/*
+   etdWebhook is a console-based message-forwarding tool. 
+   etdWebhook forwards text_bodies (email) to a discord webhook. 
+
+   Copyright (C) 2024  Carl Öttinger (Carl Oettinger)
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+   You can contact me in the following ways:
+       EMail: oettinger.carl@web.de or big-programming@web.de
+
+   issues and bugs can be reported on https://guthub.com/faolanbig/etdwebhook/issues/new
+*/
+
+
+using MailKit.Net.Imap;
 using MailKit.Search;
 using MimeKit;
 using Serilog;
